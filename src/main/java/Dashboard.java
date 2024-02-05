6407617369cbd6a4c44aa59c2bc4bd3f6f52cfaf
Sophal Chhay <134204301+chhay-sophal@users.noreply.github.com>
2024-02-05 18:08:28 +0700

    Display table of staff in dashboard

--- src/main/java/Dashboard.java
+++ src/main/java/Dashboard.java
@@ -1,8 +1,11 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
 import java.sql.SQLException;
+import java.util.Vector;
 
 public class Dashboard extends JFrame {
     private static final String APPLICATIONS_FILE = "config.properties";
@@ -10,6 +13,7 @@ public class Dashboard extends JFrame {
     private JTabbedPane tabbedPane1;
     private JButton createAStaffAccountButton;
     private JTable tableStaff;
+    private DefaultTableModel staffTableModel;
 
     public Dashboard() {
 
@@ -22,6 +26,34 @@ public class Dashboard extends JFrame {
                 dialog.setVisible(true);
             }
         });
+
+        // Initialize table model and set it to the table
+//        staffTableModel = new DefaultTableModel();
+        String[] columnNames = {"ID", "Username",};
+        staffTableModel = new DefaultTableModel(columnNames, 0);
+        System.out.print(staffTableModel.getColumnName(1));
+        tableStaff.setModel(staffTableModel);
+        // Fetch staff data from the database
+        fetchStaffData();
+    }
+
+    private void fetchStaffData() {
+        try (Connection connection = DatabaseHelper.getConnection()) {
+            assert connection != null;
+            try (Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery("SELECT * FROM users where user_type = 1")) {
+                while (resultSet.next()) {
+                        Vector<Object> row = new Vector<>();
+                        row.add(resultSet.getInt("id"));
+                        row.add(resultSet.getString("username"));
+                        // Add more columns as needed
+
+                        staffTableModel.addRow(row);
+                    }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     private static int getRowCount(ResultSet resultSet) throws SQLException {
